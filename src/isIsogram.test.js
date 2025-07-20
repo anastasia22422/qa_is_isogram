6f26689d43429a9afe09a170e583dcdc9c4f8a87
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-20 13:07:32 +0200

    Update isIsogram.test.js

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true if string is empty', () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return correct type of value', () => {
+    const result = isIsogram('');
+
+    expect(typeof result).toBe('boolean');
+  });
+
+  it('should return correct type of value', () => {
+    const result = isIsogram('');
+
+    expect(typeof result).toBe('boolean');
+  });
+
+  it('should return true for correct value', () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for incorrect value', () => {
+    const result = isIsogram('oops');
+
+    expect(result).toBe(false);
+  });
 });
